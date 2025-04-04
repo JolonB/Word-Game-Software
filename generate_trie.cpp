e737159adda96a6bc7d160ca77e7a6d4479e3a7e
JolonB <jolon.behrent@gmail.com>
2025-04-04 21:23:09 +1300

    Add ability to write trie to disk
    Overall, this can reduce a 1.9MB word list down to 1.55MB. Not really that much of an improvement, but it probably will be faster and is smaller nonetheless

--- generate_trie.cpp
+++ generate_trie.cpp
@@ -2,35 +2,65 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 #include "trienode.h"
 
-std::vector<TrieNode> trie;
+// std::vector<TrieNode> trie;
 
-bool addNode(int index, char* line, size_t strlen)
+TrieNode* findChild(const TrieNode * const node, char childLetter)
 {
-    // TODO add node if it doesn't already exist
-
-    TrieNode* node;
-    node = &trie[index];
-
-    char firstChar = line[0];
-    int nChildren = node->nChildren;
-    int firstChild = node->firstChild;
-
-    int childIndex = 0;
-    for (childIndex = 0; childIndex < nChildren; childIndex++)
+    for (TrieNode* child : node->children)
     {
-        int nextIndex = firstChild + childIndex;
-        TrieNode* nextNode = &trie[nextIndex];
-        if (nextNode->letter == firstChar)
+        if (child->letter == childLetter)
         {
-            addNode(nextIndex, line+1, strlen-1);
+            return child;
         }
     }
+    return nullptr;
 }
 
-bool parseFile(std::string filename)
+TrieNode* createChild(TrieNode* parent, char letter, bool isWord)
+{
+    TrieNode* node = new TrieNode();
+    node->letter = letter;
+    node->isWord = isWord;
+    parent->children.push_back(node);
+    return node;
+}
+
+bool addNode(TrieNode* node, const char* line, size_t strlen)
+{
+    char firstChar = line[0];
+    // If at the last character, create a node
+    if (strlen == 1)
+    {
+        createChild(node, firstChar, true);
+        return true;
+    }
+
+    TrieNode* nextChild = findChild(node, firstChar);
+    // Create child if it doesn't exist
+    if (nextChild == nullptr)
+    {
+        nextChild = createChild(node, firstChar, false);
+    }
+    addNode(nextChild, line+1, strlen-1);
+
+    return true;
+}
+
+void trimString(std::string& str)
+{
+    // Remove \n and \r from string
+    size_t strLen = str.size();
+    if (!str.empty() && str[strLen - 1] == '\n')
+        str.erase(strLen - 1);
+    if (!str.empty() && str[strLen - 1] == '\r')
+        str.erase(strLen - 1);
+}
+
+bool parseFile(std::string filename, TrieNode* const root)
 {
    std::ifstream file(filename);
    if (!file)
@@ -42,16 +72,120 @@ bool parseFile(std::string filename)
    std::string line;
    while (std::getline(file, line))
    {
-    std::cout << line << std::endl;
+    trimString(line);
+    // std::cout << line << std::endl;
+    addNode(root, line.c_str(), line.length());
    }
 
    file.close();
    return true;
 }
 
+void printTree(const TrieNode* const root, int level=0)
+{
+    for (int i = 0; i < level; i++) std::cout << " ";
+    std::cout << root->letter << " " << (root->isWord ? "-" : "") << level << std::endl;
+    for (const TrieNode* const node : root->children)
+    {
+        printTree(node, level+1);
+    }
+}
+
+bool isWordInTree(const TrieNode* const root, const char* word, int strlen)
+{
+    const TrieNode* const child = findChild(root, word[0]);
+    if (child == nullptr) {
+        return false;
+    }
+    if (strlen == 1 && child->isWord)
+    {
+        return true;
+    }
+    return isWordInTree(child, word+1, strlen-1);
+}
+
+bool isWordInTree(const TrieNode* const root, std::string word)
+{
+    return isWordInTree(root, word.c_str(), word.length());
+}
+
+void writeNode(std::ofstream& file, const TrieNode* const node, int childOffset)
+{
+    int nChildren = node->children.size();
+    // Convert TrieNode to StaticTrieNode
+    StaticTrieNode stnode;
+    stnode.letterID = node->letter - START_CHAR;
+    stnode.nChildren = nChildren;
+    stnode.firstChild = (nChildren > 0) ? childOffset : 0;
+    stnode.isWord = node->isWord;
+    file.write(reinterpret_cast<char*>(&stnode), sizeof(StaticTrieNode));
+}
+
+void writeBreadthFirst(std::ofstream& file, const TrieNode* const root)
+{
+    std::queue<const TrieNode*> inputNodes;
+    inputNodes.push(root);
+
+    int maxLen = 0;
+    while (!inputNodes.empty())
+    {
+        // Write first node in queue
+        const TrieNode* node = inputNodes.front();
+        writeNode(file, node, inputNodes.size());
+        if (inputNodes.size() > maxLen) maxLen = inputNodes.size();
+        // Remove from queue now that it's been processed
+        inputNodes.pop();
+
+        // Add children to queue
+        for (const TrieNode* const child : node->children)
+        {
+            inputNodes.push(child);
+        }
+    }
+    std::cout << "Max len: " << maxLen << std::endl;
+}
+
+void writeTrieToDisk(std::string filename, const TrieNode* const root)
+{
+    std::ofstream file(filename, std::ios::binary);
+    writeBreadthFirst(file, root);
+
+    file.close();
+}
+
+bool checkWord(std::ifstream& trie, std::string word)
+{
+    // Return to start of trie
+    trie.seek(0);
+    // Loop over each child until we find the one we want
+    // TODO
+    return false;
+}
+
+#define ISWORD(x) std::cout << (x) << " " << isWordInTree(&root, (x)) << std::endl
+#define ISWORDDISK(x) std::cout << (x) << " " << checkWord(trie, (x)) << std::endl
+
 int main()
 {
     TrieNode root;
-    trie.push_back(root);
-    return !parseFile("wordlist.txt");
+    // trie.push_back(root);
+    bool parseSuccess = parseFile("twl.txt", &root);
+    // printTree(&root);
+    ISWORD("hello");
+    ISWORD("cat");
+    ISWORD("car");
+    ISWORD("cart");
+    ISWORD("bedding");
+    ISWORD("jolon");
+    ISWORD("bet");
+    ISWORD("bets");
+    ISWORD("aa");
+    ISWORD("a");
+    ISWORD("i");
+    ISWORD("aaron");
+    writeTrieToDisk("trie.bin", &root);
+
+    std::ifstream trie("trie.bin", std::ios::binary);
+    ISWORDDISK("hello");
+    return !parseSuccess;
 }
--- trienode.h
+++ trienode.h
@@ -1,8 +1,27 @@
+#include <vector>
+
+#define START_CHAR ('a' - 1);
 
 struct TrieNode
 {
-    char letter ='\0';
-    uint8_t nChildren = 0;
-    uint32_t firstChild = 1;
+    char letter = START_CHAR;
+    std::vector<TrieNode*> children;
     uint8_t isWord = 0;  // bool
 };
+
+struct StaticTrieNode
+{
+    uint32_t letterID: 6;
+    uint32_t nChildren: 5;
+    uint32_t firstChild: 20;
+    uint32_t isWord: 1;  // bool
+};
+
+/*
+letterID = [1.2:1.7]
+nChildren = [2.5:2.7 + 1.0:1.1]
+firstChild = [4.1:4.7 + 3 + 2.0:2:4]
+isWord = [4.0]
+*/
+
+// TODO Remember endianness matters here, so I may need to change how it is written to disk
